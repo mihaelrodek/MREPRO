6980965ecbfa8f52a166d1c2f0966d9f2fa09005
Mihael Rodek <mihael.rodek@fer.hr>
2021-05-13 14:34:17 +0200

    Izmijena za ispitavanje labosa

--- lab2/bot.c
+++ lab2/bot.c
@@ -17,6 +17,8 @@
 #include <time.h> 
 
 #define MAXLEN 1024
+#define N 100
+#define M 20
 
 struct IP_port {
 	char IP[INET_ADDRSTRLEN];
@@ -36,17 +38,23 @@ int main(int argc, char *argv[]){
 	struct addrinfo hints, hints2, *res, *msgres;
 	struct sockaddr serveraddr, addr;
 	socklen_t serveraddrlen, addrlen;
+	char payloadPair[MAXLEN];
 
-
-	int CCport, recieved;
-	char IP[INET_ADDRSTRLEN];
-
+	int CCport, recieved, counter=0,recievedPORT;
+	char IP[INET_ADDRSTRLEN],recievedIP[INET_ADDRSTRLEN];
     char msg[MAXLEN] = {0};
 
 	char *reg = "REG\n", *hello = "HELLO\n", *hostname, *port;
     char command;
     
-	struct MSG message;
+	struct MSG message;	
+	struct MSG victimMessage;	
+	fd_set readset;
+	fd_set writeset;
+	
+	struct timeval tv;
+    tv.tv_sec = 2;
+    tv.tv_usec = 500000;
 	
 	if (argc!=3){
 		err(3,"Usage: ./bot ip port");
@@ -100,6 +108,12 @@ int main(int argc, char *argv[]){
 		}else if(command=='1'){
 			
 			memset(&msg[0], 0, sizeof(msg));
+			
+			memset(&hints2, 0, sizeof(hints2));
+			hints2.ai_family = AF_INET;
+			hints2.ai_socktype = SOCK_STREAM;
+			hints2.ai_flags |= AI_CANONNAME;
+			Getaddrinfo(hostname, port, &hints2, &msgres);
 
 			Getaddrinfo(&message.ip_pairs[0].IP[0],&message.ip_pairs[0].PORT[0],&hints2,&msgres);
 			
@@ -108,9 +122,8 @@ int main(int argc, char *argv[]){
 			
 			recieved = send(myTCPSock, hello, strlen(hello), 0);
 
-
 			msgLen = recv(myTCPSock, &msg[0], MAXLEN, 0);
-			//printf("Recieved: '%s'\n", msg);
+			printf("Recieved: %s \n", msg);
 			
 			close(myTCPSock);
 
@@ -122,20 +135,76 @@ int main(int argc, char *argv[]){
 
 			memset(&msg[0], 0, sizeof(msg));
 			
-			msgLen = recvfrom(mySock, &msg, MAXLEN, 0, &serveraddr, &serveraddrlen);
-			//printf("Recieved: '%s'\n", recMsg);
+			Recvfrom(mySock, &msg, MAXLEN, 0, &serveraddr, &serveraddrlen);
+			printf("Recieved: %s \n", msg);
 		}else if(command=='3'){
-			//Jel mi mozete objasniti na koji nacin napisati za 3 i 4 da ispravim do labosa?
+			
+			
+			//printf("tu sam\n");
+			
+			
+					
+					for(int j = 0; j< N; j++){
+						//printf("pocetak j %d \n",j);
+						int messlen= strlen(msg);
+						if(messlen<=0){
+							printf("Error while recieving message\n");
+							break;
+						}
+													
+						for (int k = 0; k < M; k++) {
+							//printf("pocetak k %d \n", k);
+							if (strcmp(&message.ip_pairs[k].IP[0], "") != 0) {	
+							//char currentIP = &message.ip_pairs[k].IP[0];
+							//char currentPORT = &message.ip_pairs[k].PORT[0];
+							//printf("Meta [%d]: %s %s\n", i, &message.ip_pairs[k].IP[0], &message.ip_pairs[k].PORT[0]);
+							Getaddrinfo(&message.ip_pairs[k].IP[0], &message.ip_pairs[k].PORT[0], &hints, &res);
+							counter = 0;
+							
+							for (int l = 0; l < strlen(msg) + 1; l++) {
+								//printf("%c \n", msg[l]);
+								if (msg[l] != ':') {
+									payloadPair[counter] = msg[l];
+									++counter;
+								} else {
+									payloadPair[counter] = '\0';
+									counter = 0;
+
+									recieved=Sendto(mySock, &payloadPair[0],strlen(payloadPair), 0,	res->ai_addr, res->ai_addrlen);
+									
+									printf("[SENT: %d], %s \n", recieved, &payloadPair[0]);
+								}
+							}		
+							}else break;
+						}
+						
+						
+
+						FD_ZERO(&readset);
+						FD_ZERO(&writeset);
+						FD_SET(mySock, &readset);
+						FD_SET(mySock, &writeset);
+									
+						Select(mySock + 1, &readset, &writeset, NULL, &tv);
+
+						if (FD_ISSET(mySock, &readset)) {
+							int rec = Recvfrom(mySock, &victimMessage, MAXLEN, 0, &addr, &addrlen);
+
+							recievedPORT = ntohs(((struct sockaddr_in *)(&addr))->sin_port);
+							inet_ntop(AF_INET, &(((struct sockaddr_in *)(&addr))->sin_addr), &recievedIP[0],INET_ADDRSTRLEN);
+							
+							if (recievedPORT != CCport || (strcmp(IP, recievedIP)) != 0) {
+								printf("Stopped!\n");
+								break;
+							}
+						}
+						sleep(1);					
+					}
+			
 		}else if(command=='4'){
-			//printf("Stopped\n");
+			printf("Stopped!\n");
 			close(mySock);
 			close(myTCPSock);
-		
-		}
-		
-		
+		}		
 	}
-	
-	
-	
 }
